27f2289c983aa927dda7721d679c6ef8faee9ee0
farahharir <46160258+farahharir@users.noreply.github.com>
2020-05-02 11:57:17 +0200

    Update Constant.java

--- src/main/java/com/ocr/P6/config/Constant.java
+++ src/main/java/com/ocr/P6/config/Constant.java
@@ -3,10 +3,10 @@ package com.ocr.P6.config;
 
 public class Constant {
 
-    public static final String MY_EMAIL = "escaladeoc@gmail.com";
+    public static final String MY_EMAIL = "";
 
 
-    public static final String MY_PASSWORD = "Mounir92@";
+    public static final String MY_PASSWORD = "";
 
 
 
